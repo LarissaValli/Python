69f94ea3b7bd6f1e363a0c628a9be746e41fb4c8
Larissa Valli <larissavalli4@gmail.com>
2026-03-26 12:02:51 -0300

    deixando tudo pronto para aprender python

---
+++ cursoemvideo/exercicios.py
@@ -0,0 +1,19 @@
+#atividade 1
+
+nome = input('Qual seu nome?')
+print('Boas vindas', nome, 'prazer em conhecer você!')
+
+#---------------------------
+
+dia = input('Qual dia você nasceu?')
+mes = input('Qual mês?')
+ano = input('E qual ano você nasceu?')
+
+print('Você nasceu:', dia, mes, ano)
+
+#---------------------------
+
+n1 = float(input('Digite um número inteiro:'))
+n2 = float(input('Digite outro número:'))
+
+print('A soma:', n1 + n2)
---
+++ cursoemvideo/python1.py
@@ -0,0 +1,7 @@
+nome = input('Qual seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Qual seu peso')
+
+print(nome, idade, peso)
+
+#aula 5 - lista de exercicio.
