2eb4279efe047ce23e659fa1f726965af85d9de2
Larissa Valli <larissavalli4@gmail.com>
2026-03-27 11:33:18 -0300

    aprendendo sobre tipos primitivos
    type()
    isnumeric()
    isapha()
    isupper()

--- cursoemvideo/exercicios.py
+++ cursoemvideo/exercicios.py
@@ -33,6 +33,31 @@ print('Olá, mundo!')
 nome = input('Qual seu nome?')
 print(f'É um prazer conhecer você,{nome}!')
 
+#aula 6 - Exercício 01
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+soma = n1 + n2
+print(f'A soma de {n1} e {n2} é: {soma}')
+
+#Exercício 2
+
+x = input('Digite algo: ')
+
+n = x.isnumeric()
+l = x.isalpha()
+a = x.isalnum()
+m = x.isupper()
+
+print(f'{x} É número? {n}')
+print(f'{x} É letra? {l}')
+print(f'{x} É alphanumerico? {a}')
+print(f'{x} É letra maiuscula? {m}')
+
+#Exercício 3
+
+
 
 
 
--- cursoemvideo/python1.py
+++ cursoemvideo/python1.py
@@ -4,4 +4,33 @@ peso = input('Qual seu peso')
 
 print(nome, idade, peso)
 
+#--------------------------
+#         Tipos primitivos
+
+n = float(input('Digite um número: '))
+
+print(n)
+
+n = input('Digite algo: ')
+print(n.isnumeric())
+#obs: Vai dizer se é possível converter o valor em um número com o tipo primitivo int antes dele.
+
+n = input('Digite algo: ')
+print(n.isalpha())
+# Se ele é letra.
+
+n = input('Digite algo: ')
+print(n.isalnum())
+# Se o valor tem número e letra. Ex: 3a (é alpha numerico)
+
+n = input('Digite algo: ')
+print(n.isupper())
+#Vai analisar se está somente com letras maiusculas.
+
+#---------------------------------------
+#           Operadores Aritméticos
+
+
+
+
 
