b92d8bad2c31757cc4955494192299a1f2f52077
Larissa Valli <larissavalli4@gmail.com>
2026-07-27 11:35:57 -0300

    Exercício parte 1 das listas feito!

--- exercicios3.py
+++ exercicios3.py
@@ -39,8 +39,17 @@ valores = []
 
 for cont in range(0, 5):
     n = int(input('Digite um valor: '))
-    
-
+    if cont == 0 or n < valores[-1]:
+        valores.append(n)
+    else:
+        pos = 0
+        while pos < len(valores):
+            if n <= valores[pos]:
+                valores.insert(pos, n)
+                break
+            pos += 1
+print('=-' * 30)
+print(f'Os valores digitados em ordem foram {valores}')
 
 #------------------------------------------
 #4)Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre: A)Quantos números foram digitados. B)A lista de valores, ordenada de forma decrescente. C) Se o valor 5 foi digitado e está ou não na lista.
@@ -64,6 +73,39 @@ while True:
 #------------------------------------------
 #5) Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão contar apenas os valores pares e os valores impares digitados, respectivamente. Ao final, mostre o conteúdo das três listas geradas.
 
+valores = []
+pares = []
+impares = []
+
+while True:
+    n = int(input('Digite um número: '))
+    valores.append(n)
+    if (n % 2 == 0):
+        pares.append(n)
+    else:
+        impares.append(n)
+    continuar = str(input('Quer continuar? [S/N] '))
+    if continuar in 'Nn':
+        break
+print('=-' * 30)
+print(f'A lista completa é {sorted(valores)}')
+print(f'A lista de pares é {sorted(pares)}')
+print(f'A lista de impares é {sorted(impares)}')
+
+#------------------------------------------
+#6) Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
 
 
 
